c7adaaf4f73d406b20555cec902ffe696fb3e079
Badr Khamis <bkhamis@ca.ibm.com>
2017-03-08 10:07:49 -0400

    play with column name

--- panda_lession1.py
+++ panda_lession1.py
@@ -3,17 +3,26 @@ import numpy as np
 
 class Panda:
     def __init__(self,path):
-        dataset = pd.read_csv(path)
+        dataset = pd.read_csv(path,header=None,names=['label','message'])
         self.__data = dataset
 
     def basic_panda(self):
         print self.__data.shape
         print self.__data.head()
+        print self.__data.dtypes
+        print type(self.__data)
+        self.__data.describe(include=['object'])
 
     def column_name(self):
-        
+        self.__data.rename(columns={'label':'Labels'},inplace=True)
+        col = ['label','message']
+        # self.__data.columns = col
 
+        col = self.__data.columns.str.replace('l','A')
+        self.__data.columns = col
+        print self.__data.head()
 if __name__ == "__main__" :
     path = '/Users/badrkhamis/learning_path/python/classfication_scitit-learn/sms.csv'
     pointer = Panda(path)
     pointer.basic_panda()
+    pointer.column_name()
